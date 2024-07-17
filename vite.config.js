f8f36a270038bb4b2cbc1d61cf28517e15d3b766
KJG04 <freedom7113@gmail.com>
2024-07-17 14:21:22 +0900

    정적 이미지 파일 오류 수정

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: path.resolve(__dirname, 'src'),
+  publicDir: path.resolve(__dirname, 'public'),
   build: {
     outDir: path.resolve(__dirname, 'dist'),
     rollupOptions: {
